b02bae1475f689240df2898ffb7bb41dc6214bf9
andavid <jiankedai@gmail.com>
2019-03-08 15:17:20 +0800

    solve 142

---
+++ vscode/142.linked-list-cycle-ii.java
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode id=142 lang=java
+ *
+ * [142] Linked List Cycle II
+ *
+ * https://leetcode.com/problems/linked-list-cycle-ii/description/
+ *
+ * algorithms
+ * Medium (30.94%)
+ * Total Accepted:    197K
+ * Total Submissions: 636.6K
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * Given a linked list, return the node where the cycle begins. If there is no
+ * cycle, return null.
+ * 
+ * To represent a cycle in the given linked list, we use an integer pos which
+ * represents the position (0-indexed) in the linked list where tail connects
+ * to. If pos is -1, then there is no cycle in the linked list.
+ * 
+ * Note: Do not modify the linked list.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: tail connects to node index 1
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * second node.
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1,2], pos = 0
+ * Output: tail connects to node index 0
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * first node.
+ * 
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = [1], pos = -1
+ * Output: no cycle
+ * Explanation: There is no cycle in the linked list.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Follow up:
+ * Can you solve it without using extra space?
+ * 
+ */
+/**
+ * Definition for singly-linked list. class ListNode { int val; ListNode next;
+ * ListNode(int x) { val = x; next = null; } }
+ */
+public class Solution {
+  public ListNode detectCycle(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+    ListNode meetNode = null;
+
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow == fast) {
+        meetNode = slow;
+        break;
+      }
+    }
+
+    if (meetNode == null) {
+      return null;
+    }
+
+    ListNode p = head;
+    ListNode q = meetNode;
+    while (p != q) {
+      p = p.next;
+      q = q.next;
+    }
+
+    return p;
+  }
+}
