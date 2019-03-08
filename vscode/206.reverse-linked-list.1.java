c660c94829d03c0cb30fccb459dfb6303c3623f0
andavid <jiankedai@gmail.com>
2019-03-08 14:59:12 +0800

    solve 206 using recursion

---
+++ vscode/206.reverse-linked-list.1.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=206 lang=java
+ *
+ * [206] Reverse Linked List
+ *
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (52.81%)
+ * Total Accepted:    520.1K
+ * Total Submissions: 984.7K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Reverse a singly linked list.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ * 
+ * 
+ * Follow up:
+ * 
+ * A linked list can be reversed either iteratively or recursively. Could you
+ * implement both?
+ * 
+ */
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+class Solution {
+  public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    ListNode node = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return node;
+  }
+}
