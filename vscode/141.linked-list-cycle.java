537c2335eb6fd812663008fa017b76773203c957
andavid <jiankedai@gmail.com>
2019-03-08 15:15:49 +0800

    solve 141

---
+++ vscode/141.linked-list-cycle.java
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode id=141 lang=java
+ *
+ * [141] Linked List Cycle
+ *
+ * https://leetcode.com/problems/linked-list-cycle/description/
+ *
+ * algorithms
+ * Easy (35.79%)
+ * Total Accepted:    365.4K
+ * Total Submissions: 1M
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * Given a linked list, determine if it has a cycle in it.
+ * 
+ * To represent a cycle in the given linked list, we use an integer pos which
+ * represents the position (0-indexed) in the linked list where tail connects
+ * to. If pos is -1, then there is no cycle in the linked list.
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * second node.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1,2], pos = 0
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * first node.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = [1], pos = -1
+ * Output: false
+ * Explanation: There is no cycle in the linked list.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Follow up:
+ * 
+ * Can you solve it using O(1) (i.e. constant) memory?
+ * 
+ */
+/**
+ * Definition for singly-linked list. class ListNode { int val; ListNode next;
+ * ListNode(int x) { val = x; next = null; } }
+ */
+public class Solution {
+  public boolean hasCycle(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow == fast) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
