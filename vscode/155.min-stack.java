22f5fdbd9594946abcaad088a270485b9781abe0
andavid <jiankedai@gmail.com>
2019-03-18 16:27:46 +0800

    solve 155

---
+++ vscode/155.min-stack.java
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=155 lang=java
+ *
+ * [155] Min Stack
+ *
+ * https://leetcode.com/problems/min-stack/description/
+ *
+ * algorithms
+ * Easy (35.70%)
+ * Total Accepted:    275.3K
+ * Total Submissions: 766.6K
+ * Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n[[],[-2],[0],[-3],[],[],[],[]]'
+ *
+ * 
+ * Design a stack that supports push, pop, top, and retrieving the minimum
+ * element in constant time.
+ * 
+ * 
+ * push(x) -- Push element x onto stack.
+ * 
+ * 
+ * pop() -- Removes the element on top of the stack.
+ * 
+ * 
+ * top() -- Get the top element.
+ * 
+ * 
+ * getMin() -- Retrieve the minimum element in the stack.
+ * 
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> Returns -3.
+ * minStack.pop();
+ * minStack.top();      --> Returns 0.
+ * minStack.getMin();   --> Returns -2.
+ * 
+ * 
+ */
+class MinStack {
+    private int min;
+    private Stack<Integer> stack;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        min = Integer.MAX_VALUE;
+        stack = new Stack<Integer>();
+    }
+    
+    public void push(int x) {
+        if (x <= min) {
+          stack.push(min);
+          min = x;
+        }
+        stack.push(x);
+    }
+    
+    public void pop() {
+        if (stack.pop() == min) {
+          min = stack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
