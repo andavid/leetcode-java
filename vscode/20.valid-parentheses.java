0c0b78814db3d0eb9c668d73df4e87687df3da20
andavid <jiankedai@gmail.com>
2019-03-18 15:01:35 +0800

    solve 20

---
+++ vscode/20.valid-parentheses.java
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode id=20 lang=java
+ *
+ * [20] Valid Parentheses
+ *
+ * https://leetcode.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (35.92%)
+ * Total Accepted:    534K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '"()"'
+ *
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ * 
+ * An input string is valid if:
+ * 
+ * 
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * 
+ * 
+ * Note that an empty string is also considered valid.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "()"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "()[]{}"
+ * Output: true
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "(]"
+ * Output: false
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: "([)]"
+ * Output: false
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: "{[]}"
+ * Output: true
+ * 
+ * 
+ */
+class Solution {
+  public boolean isValid(String s) {
+    Stack<Character> stack = new Stack<>();
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if (c == '(') {
+        stack.push(')');
+      } else if (c == '{') {
+        stack.push('}');
+      } else if (c == '[') {
+        stack.push(']');
+      } else if (stack.empty() || stack.pop() != c) {
+        return false;
+      }
+    }
+    return stack.empty();
+  }
+}
+
