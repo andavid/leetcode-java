d3f8acd6497e8da324f3b4cf8ac37462fa22c28e
andavid <jiankedai@gmail.com>
2019-03-27 17:16:36 +0800

    solve 94: binary tree inorder traversal using iteration

---
+++ vscode/94.binary-tree-inorder-traversal.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+/*
+ * @lc app=leetcode id=94 lang=java
+ *
+ * [94] Binary Tree Inorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (55.10%)
+ * Total Accepted:    426.4K
+ * Total Submissions: 767.5K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,null,2,3]
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ * 
+ * Output: [1,3,2]
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> list = new ArrayList<>();
+    Stack<TreeNode> stack = new Stack<>();
+    TreeNode p = root;
+
+    while (!stack.empty() || p != null) {
+      while (p != null) {
+        stack.push(p);
+        p = p.left;
+      }
+      if (!stack.empty()) {
+        p = stack.pop();
+        list.add(p.val);
+        p = p.right;
+      }
+    }
+
+    return list;
+  }
+}
+
