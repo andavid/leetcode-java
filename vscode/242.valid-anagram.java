789f0709bcc97f4d60f3a24114002d038954e6ef
andavid <jiankedai@gmail.com>
2019-03-30 20:36:56 +0800

    solve 242: valid anagram

---
+++ vscode/242.valid-anagram.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=242 lang=java
+ *
+ * [242] Valid Anagram
+ *
+ * https://leetcode.com/problems/valid-anagram/description/
+ *
+ * algorithms
+ * Easy (50.97%)
+ * Total Accepted:    315K
+ * Total Submissions: 613.4K
+ * Testcase Example:  '"anagram"\n"nagaram"'
+ *
+ * Given two strings s and t , write a function to determine if t is an anagram
+ * of s.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "rat", t = "car"
+ * Output: false
+ * 
+ * 
+ * Note:
+ * You may assume the string contains only lowercase alphabets.
+ * 
+ * Follow up:
+ * What if the inputs contain unicode characters? How would you adapt your
+ * solution to such case?
+ * 
+ */
+class Solution {
+  public boolean isAnagram(String s, String t) {
+    int[] counts = new int[26];
+    for (char c : s.toCharArray()) {
+      counts[c - 'a']++;
+    }
+    for (char c : t.toCharArray()) {
+      counts[c - 'a']--;
+    }
+    for (int count : counts) {
+      if (count != 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
