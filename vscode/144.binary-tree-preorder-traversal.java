989fb42b43aad105cc29da84736a29e7b560de18
andavid <jiankedai@gmail.com>
2019-04-01 17:18:12 +0800

    solve 144: binary tree preorder traversal using iteration

---
+++ vscode/144.binary-tree-preorder-traversal.java
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=144 lang=java
+ *
+ * [144] Binary Tree Preorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Medium (50.28%)
+ * Total Accepted:    317.6K
+ * Total Submissions: 626.7K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,null,2,3]
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ * 
+ * Output: [1,2,3]
+ * 
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public List<Integer> preorderTraversal(TreeNode root) {
+    List<Integer> list = new ArrayList<>();
+    Stack<TreeNode> stack = new Stack<>();
+    TreeNode p = root;
+
+    while (!stack.empty() || p != null) {
+      while (p != null) {
+        list.add(p.val);
+        stack.push(p);
+        p = p.left;
+      }
+      p = stack.pop();
+      p = p.right;
+    }
+
+    return list;
+  }
+}
+
