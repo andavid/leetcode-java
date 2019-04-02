868fc058ec7463abdc810e544a08004e8a256a0b
andavid <jiankedai@gmail.com>
2019-04-02 22:20:16 +0800

    solve 145: binary tree postorder traversal

---
+++ vscode/145.binary-tree-postorder-traversal.java
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode id=145 lang=java
+ *
+ * [145] Binary Tree Postorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Hard (46.93%)
+ * Total Accepted:    245.5K
+ * Total Submissions: 516.7K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given a binary tree, return the postorder traversal of its nodes' values.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,null,2,3]
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ * 
+ * Output: [3,2,1]
+ * 
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public List<Integer> postorderTraversal(TreeNode root) {
+    List<Integer> list = new ArrayList<>();
+    Stack<TreeNode> stack = new Stack<>();
+    TreeNode cur = root;
+    TreeNode last = null;
+    while (cur != null || !stack.empty()) {
+      while (cur != null) {
+        stack.push(cur);
+        cur = cur.left;
+      }
+      cur = stack.peek();
+      if (cur.right == null || cur.right == last) {
+        list.add(cur.val);
+        stack.pop();
+        last = cur;
+        cur = null;
+      } else {
+        cur = cur.right;
+      }
+    }
+    return list;
+  }
+}
+
