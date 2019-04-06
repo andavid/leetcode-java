f2c05a78e9eea2e7dbdd3454b88823bde48fe6b6
andavid <jiankedai@gmail.com>
2019-04-07 00:33:10 +0800

    solve 101: symmetric tree

---
+++ vscode/101.symmetric-tree.java
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=101 lang=java
+ *
+ * [101] Symmetric Tree
+ *
+ * https://leetcode.com/problems/symmetric-tree/description/
+ *
+ * algorithms
+ * Easy (42.73%)
+ * Total Accepted:    378K
+ * Total Submissions: 878.2K
+ * Testcase Example:  '[1,2,2,3,4,4,3]'
+ *
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric
+ * around its center).
+ * 
+ * 
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠/ \ / \
+ * 3  4 4  3
+ * 
+ * 
+ * 
+ * But the following [1,2,2,null,3,null,3]  is not:
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠  \   \
+ * ⁠  3    3
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * Bonus points if you could solve it both recursively and iteratively.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public boolean isSymmetric(TreeNode root) {
+    if (root == null) {
+      return true;
+    }
+    return isSym(root.left, root.right);
+  }
+
+  public boolean isSym(TreeNode r1, TreeNode r2) {
+    if (r1 == null && r2 == null) {
+      return true;
+    }
+    if (r1 == null || r2 == null) {
+      return false;
+    }
+    return r1.val == r2.val && isSym(r1.left, r2.right) && isSym(r1.right, r2.left);
+  }
+}
+
