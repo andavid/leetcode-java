45a1cb1039b1b1cf08442e3b8d499b4eec2c76d7
andavid <jiankedai@gmail.com>
2019-04-11 15:50:31 +0800

    solve 7: reverse integer

---
+++ vscode/7.reverse-integer.java
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=7 lang=java
+ *
+ * [7] Reverse Integer
+ *
+ * https://leetcode.com/problems/reverse-integer/description/
+ *
+ * algorithms
+ * Easy (25.14%)
+ * Total Accepted:    653.5K
+ * Total Submissions: 2.6M
+ * Testcase Example:  '123'
+ *
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 123
+ * Output: 321
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: -123
+ * Output: -321
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 120
+ * Output: 21
+ * 
+ * 
+ * Note:
+ * Assume we are dealing with an environment which could only store integers
+ * within the 32-bit signed integer range: [−2^31,  2^31 − 1]. For the purpose
+ * of this problem, assume that your function returns 0 when the reversed
+ * integer overflows.
+ * 
+ */
+class Solution {
+  public int reverse(int x) {
+    long result = 0;
+    while (x != 0) {
+      result = result * 10 + x % 10;
+      x = x / 10;
+    }
+    if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+      return 0;
+    }
+    return (int)result;
+  }
+}
+
