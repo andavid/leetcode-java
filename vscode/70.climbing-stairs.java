45a9fe14cdf7050c34e02400910231332d607e9b
andavid <jiankedai@gmail.com>
2019-04-12 16:28:28 +0800

    solve 70: climbing stairs

---
+++ vscode/70.climbing-stairs.java
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=70 lang=java
+ *
+ * [70] Climbing Stairs
+ *
+ * https://leetcode.com/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (43.48%)
+ * Total Accepted:    381.1K
+ * Total Submissions: 869K
+ * Testcase Example:  '2'
+ *
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ * 
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can
+ * you climb to the top?
+ * 
+ * Note: Given n will be a positive integer.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ * 
+ * 
+ */
+class Solution {
+  public int climbStairs(int n) {
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+
+    int one_step_before = 2;
+    int two_steps_before = 1;
+    int result = 0;
+
+    for (int i = 2; i < n; i++) {
+      result = one_step_before + two_steps_before;
+      two_steps_before = one_step_before;
+      one_step_before = result;
+    }
+    
+    return result;
+  }
+}
+
