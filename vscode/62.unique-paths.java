8b299479e36c710b3d7e48e0062468a61931a702
andavid <jiankedai@gmail.com>
2019-04-12 16:47:30 +0800

    solve 62: unique paths

---
+++ vscode/62.unique-paths.java
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=62 lang=java
+ *
+ * [62] Unique Paths
+ *
+ * https://leetcode.com/problems/unique-paths/description/
+ *
+ * algorithms
+ * Medium (46.42%)
+ * Total Accepted:    274.4K
+ * Total Submissions: 583.7K
+ * Testcase Example:  '3\n2'
+ *
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in
+ * the diagram below).
+ * 
+ * The robot can only move either down or right at any point in time. The robot
+ * is trying to reach the bottom-right corner of the grid (marked 'Finish' in
+ * the diagram below).
+ * 
+ * How many possible unique paths are there?
+ * 
+ * 
+ * Above is a 7 x 3 grid. How many possible unique paths are there?
+ * 
+ * Note: m and n will be at most 100.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: m = 3, n = 2
+ * Output: 3
+ * Explanation:
+ * From the top-left corner, there are a total of 3 ways to reach the
+ * bottom-right corner:
+ * 1. Right -> Right -> Down
+ * 2. Right -> Down -> Right
+ * 3. Down -> Right -> Right
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: m = 7, n = 3
+ * Output: 28
+ * 
+ */
+class Solution {
+  public int uniquePaths(int m, int n) {
+    int[][] dp = new int[m][n];
+    for (int i = 0; i < n; i++) {
+      dp[m-1][i] = 1;
+    }
+    for (int i = 0; i < m; i++) {
+      dp[i][n-1] = 1;
+    }
+
+    for (int i = m - 2; i >= 0; i--) {
+      for (int j = n - 2; j >= 0; j--) {
+        dp[i][j] = dp[i+1][j] + dp[i][j+1];
+      }
+    }
+
+    return dp[0][0];
+  }
+}
+
