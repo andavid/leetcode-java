1c166df67cf54cd860cb2822f39ca14ce95c94eb
andavid <jiankedai@gmail.com>
2019-04-13 23:16:30 +0800

    solve 88: merge sorted array

---
+++ vscode/88.merge-sorted-array.java
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=88 lang=java
+ *
+ * [88] Merge Sorted Array
+ *
+ * https://leetcode.com/problems/merge-sorted-array/description/
+ *
+ * algorithms
+ * Easy (34.87%)
+ * Total Accepted:    348K
+ * Total Submissions: 985.6K
+ * Testcase Example:  '[1,2,3,0,0,0]\n3\n[2,5,6]\n3'
+ *
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as
+ * one sorted array.
+ * 
+ * Note:
+ * 
+ * 
+ * The number of elements initialized in nums1 and nums2 are m and n
+ * respectively.
+ * You may assume that nums1 has enough space (size that is greater or equal to
+ * m + n) to hold additional elements from nums2.
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input:
+ * nums1 = [1,2,3,0,0,0], m = 3
+ * nums2 = [2,5,6],       n = 3
+ * 
+ * Output: [1,2,2,3,5,6]
+ * 
+ * 
+ */
+class Solution {
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+    int i = m - 1;
+    int j = n - 1;
+    int k = m + n - 1;
+
+    while (i >= 0 && j >= 0) {
+      nums1[k--] = (nums1[i] > nums2[j]) ? nums1[i--] : nums2[j--];
+    }
+
+    while (j >= 0) {
+      nums1[k--] = nums2[j--];
+    }
+  }
+}
+
