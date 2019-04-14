e43e5ae6b01ca0c81f04b54bf2187f4c3ecd995a
andavid <jiankedai@gmail.com>
2019-04-14 22:39:47 +0800

    solve 231: power of two

---
+++ vscode/231.power-of-two.java
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=231 lang=java
+ *
+ * [231] Power of Two
+ *
+ * https://leetcode.com/problems/power-of-two/description/
+ *
+ * algorithms
+ * Easy (41.58%)
+ * Total Accepted:    211.8K
+ * Total Submissions: 509.3K
+ * Testcase Example:  '1'
+ *
+ * Given an integer, write a function to determine if it is a power of two.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1
+ * Output: true 
+ * Explanation: 20 = 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 16
+ * Output: true
+ * Explanation: 24 = 16
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 218
+ * Output: false
+ * 
+ */
+class Solution {
+  public boolean isPowerOfTwo(int n) {
+    if (n <= 0) return false;
+    if ((n & (n-1)) == 0) {
+      return true;
+    }
+    return false;
+  }
+}
+
