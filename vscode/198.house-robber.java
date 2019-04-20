510fb43c29b33126ef4691b372c3e1f4f05c6e9f
andavid <jiankedai@gmail.com>
2019-04-20 23:51:32 +0800

    solve 198.house-robber

---
+++ vscode/198.house-robber.java
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=198 lang=java
+ *
+ * [198] House Robber
+ *
+ * https://leetcode.com/problems/house-robber/description/
+ *
+ * algorithms
+ * Easy (40.78%)
+ * Total Accepted:    308.4K
+ * Total Submissions: 754.1K
+ * Testcase Example:  '[1,2,3,1]'
+ *
+ * You are a professional robber planning to rob houses along a street. Each
+ * house has a certain amount of money stashed, the only constraint stopping
+ * you from robbing each of them is that adjacent houses have security system
+ * connected and it will automatically contact the police if two adjacent
+ * houses were broken into on the same night.
+ *
+ * Given a list of non-negative integers representing the amount of money of
+ * each house, determine the maximum amount of money you can rob tonight
+ * without alerting the police.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [1,2,3,1]
+ * Output: 4
+ * Explanation: Rob house 1 (money = 1) and then rob house 3 (money =
+ * 3).
+ * Total amount you can rob = 1 + 3 = 4.
+ *
+ * Example 2:
+ *
+ *
+ * Input: [2,7,9,3,1]
+ * Output: 12
+ * Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house
+ * 5 (money = 1).
+ * Total amount you can rob = 2 + 9 + 1 = 12.
+ *
+ *
+ */
+class Solution {
+  public int rob(int[] nums) {
+    if (nums == null || nums.length == 0) return 0;
+
+    int n = nums.length;
+    if (n == 1) return nums[0];
+
+    int[] dp = new int[n];
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+
+    for (int i = 2; i < n; i++) {
+      dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
+    }
+
+    return dp[n-1];
+  }
+}
+
