866efd8c8cf874131f7edeb318894ea90b3adddd
andavid <jiankedai@gmail.com>
2019-04-21 14:57:45 +0800

    solve 326.power-of-three

---
+++ vscode/326.power-of-three.java
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=326 lang=java
+ *
+ * [326] Power of Three
+ *
+ * https://leetcode.com/problems/power-of-three/description/
+ *
+ * algorithms
+ * Easy (41.40%)
+ * Total Accepted:    178.1K
+ * Total Submissions: 428.8K
+ * Testcase Example:  '27'
+ *
+ * Given an integer, write a function to determine if it is a power of three.
+ *
+ * Example 1:
+ *
+ *
+ * Input: 27
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: 0
+ * Output: false
+ *
+ * Example 3:
+ *
+ *
+ * Input: 9
+ * Output: true
+ *
+ * Example 4:
+ *
+ *
+ * Input: 45
+ * Output: false
+ *
+ * Follow up:
+ * Could you do it without using any loop / recursion?
+ */
+class Solution {
+  public boolean isPowerOfThree(int n) {
+    if (n < 1) return false;
+    while (n % 3 == 0) {
+      n /= 3;
+    }
+    return n == 1;
+  }
+}
+
