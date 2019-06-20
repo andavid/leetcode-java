7accbe4703361fa08c95c29aaca3457c13996f5d
andavid <jiankedai@gmail.com>
2019-06-20 17:00:42 +0800

    solve 215.kth-largest-element-in-an-array

--- vscode/215.kth-largest-element-in-an-array.java
+++ vscode/215.kth-largest-element-in-an-array.java
@@ -32,34 +32,34 @@
  *
  */
 class Solution {
-  public int findKthLargest(int[] nums, int k) {
-    return partition(nums, k, 0, nums.length - 1);
+  public int findKthLargest(int[] data, int k) {
+    return partition(data, k, 0, data.length - 1);
   }
 
-  public int partition(int[] nums, int k, int p, int r) {
-    int pivot = nums[r];
-    int i = p;
-
-    for (int j = p; j < r; j++) {
-      if (nums[j] > pivot) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+  public int partition(int[] data, int k, int low, int high) {
+    int pivot = data[high];
+    int i = low;
+    for (int j = low; j < high; j++) {
+      if (data[j] > pivot) {
+        swap(data, i, j);
         i++;
       }
     }
-
-    int temp = nums[i];
-    nums[i] = nums[r];
-    nums[r] = temp;
+    swap(data, i, high);
 
     if (i + 1 == k) {
-      return nums[i];
+      return data[i];
     } else if (i + 1 < k) {
-      return partition(nums, k, i + 1, r);
+      return partition(data, k, i + 1, high);
     } else {
-      return partition(nums, k, p, i - 1);
+      return partition(data, k, low, i - 1);
     }
+  }
+
+  public void swap(int[] data, int i, int j) {
+    int temp = data[i];
+    data[i] = data[j];
+    data[j] = temp;
   }
 }
 
