4c4b36c31a7d37208ef1851dc4fe8c5f03bde7d7
andavid <jiankedai@gmail.com>
2019-06-24 11:08:27 +0800

    solve 96.unique-binary-search-trees

---
+++ vscode/96.unique-binary-search-trees.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=96 lang=java
+ *
+ * [96] Unique Binary Search Trees
+ */
+class Solution {
+  public int numTrees(int n) {
+    int[] dp = new int[n+1];
+    dp[0] = dp[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+      for (int j = 1; j <= i; j++) {
+        dp[i] += dp[j-1] * dp[i-j];
+      }
+    }
+
+    return dp[n];
+  }
+}
+
