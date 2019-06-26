d58940ef836b261e1c5181c4778810301dce25a8
andavid <jiankedai@gmail.com>
2019-06-26 15:58:00 +0800

    solve 513.find-bottom-left-tree-value

---
+++ vscode/513.find-bottom-left-tree-value.java
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode id=513 lang=java
+ *
+ * [513] Find Bottom Left Tree Value
+ *
+ * https://leetcode.com/problems/find-bottom-left-tree-value/description/
+ *
+ * algorithms
+ * Medium (58.26%)
+ * Likes:    577
+ * Dislikes: 98
+ * Total Accepted:    73.4K
+ * Total Submissions: 125.1K
+ * Testcase Example:  '[2,1,3]'
+ *
+ *
+ * Given a binary tree, find the leftmost value in the last row of the tree.
+ *
+ *
+ * Example 1:
+ *
+ * Input:
+ *
+ * ⁠   2
+ * ⁠  / \
+ * ⁠ 1   3
+ *
+ * Output:
+ * 1
+ *
+ *
+ *
+ * ⁠ Example 2:
+ *
+ * Input:
+ *
+ * ⁠       1
+ * ⁠      / \
+ * ⁠     2   3
+ * ⁠    /   / \
+ * ⁠   4   5   6
+ * ⁠      /
+ * ⁠     7
+ *
+ * Output:
+ * 7
+ *
+ *
+ *
+ * Note:
+ * You may assume the tree (i.e., the given root node) is not NULL.
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public int findBottomLeftValue(TreeNode root) {
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    int value = -1;
+
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      for (int i = 0; i < size; i++) {
+        TreeNode node = queue.poll();
+        if (i == 0) {
+          value = node.val;
+        }
+        if (node.left != null) {
+          queue.offer(node.left);
+        }
+        if (node.right != null) {
+          queue.offer(node.right);
+        }
+      }
+    }
+
+    return value;
+  }
+}
+
