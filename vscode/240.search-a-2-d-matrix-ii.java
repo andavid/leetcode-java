4cb328b2fd1f28a581c278b7c8be4c74a6cb21c7
andavid <jiankedai@gmail.com>
2019-08-30 16:55:41 +0800

    solve 240.search-a-2-d-matrix-ii

---
+++ vscode/240.search-a-2-d-matrix-ii.java
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=240 lang=java
+ *
+ * [240] Search a 2D Matrix II
+ *
+ * https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+ *
+ * algorithms
+ * Medium (41.27%)
+ * Likes:    1722
+ * Dislikes: 50
+ * Total Accepted:    200.2K
+ * Total Submissions: 484.8K
+ * Testcase Example:  '[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]\n5'
+ *
+ * Write an efficient algorithm that searches for a value in an m x n matrix.
+ * This matrix has the following properties:
+ *
+ *
+ * Integers in each row are sorted in ascending from left to right.
+ * Integers in each column are sorted in ascending from top to bottom.
+ *
+ *
+ * Example:
+ *
+ * Consider the following matrix:
+ *
+ *
+ * [
+ * ⁠ [1,   4,  7, 11, 15],
+ * ⁠ [2,   5,  8, 12, 19],
+ * ⁠ [3,   6,  9, 16, 22],
+ * ⁠ [10, 13, 14, 17, 24],
+ * ⁠ [18, 21, 23, 26, 30]
+ * ]
+ *
+ *
+ * Given target = 5, return true.
+ *
+ * Given target = 20, return false.
+ *
+ */
+class Solution {
+  public boolean searchMatrix(int[][] matrix, int target) {
+    if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+      return false;
+    }
+
+    int rowMax = matrix.length - 1;
+    int colMax = matrix[0].length - 1;
+    int rowCur = 0;
+    int colCur = colMax;
+
+    while (rowCur <= rowMax && colCur >= 0) {
+      if (matrix[rowCur][colCur] == target) {
+        return true;
+      } else if (matrix[rowCur][colCur] > target) {
+        colCur--;
+      } else {
+        rowCur++;
+      }
+    }
+
+    return false;
+  }
+}
+
