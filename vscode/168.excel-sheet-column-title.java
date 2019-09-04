6580e24e4c63437b8d52b1e0465978399dc3b78e
andavid <jiankedai@gmail.com>
2019-09-04 16:24:10 +0800

    solve 168.excel-sheet-column-title

---
+++ vscode/168.excel-sheet-column-title.java
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode id=168 lang=java
+ *
+ * [168] Excel Sheet Column Title
+ *
+ * https://leetcode.com/problems/excel-sheet-column-title/description/
+ *
+ * algorithms
+ * Easy (29.44%)
+ * Likes:    810
+ * Dislikes: 164
+ * Total Accepted:    182.7K
+ * Total Submissions: 620K
+ * Testcase Example:  '1'
+ *
+ * Given a positive integer, return its corresponding column title as appear in
+ * an Excel sheet.
+ *
+ * For example:
+ *
+ *
+ * ⁠   1 -> A
+ * ⁠   2 -> B
+ * ⁠   3 -> C
+ * ⁠   ...
+ * ⁠   26 -> Z
+ * ⁠   27 -> AA
+ * ⁠   28 -> AB
+ * ⁠   ...
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: 1
+ * Output: "A"
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: 28
+ * Output: "AB"
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: 701
+ * Output: "ZY"
+ *
+ */
+class Solution {
+  public String convertToTitle(int n) {
+    StringBuilder sb = new StringBuilder();
+    while (n != 0) {
+      sb.insert(0, (char)((n-1)%26 + 'A'));
+      n = (n-1) / 26;
+    }
+    return sb.toString();
+  }
+}
+
