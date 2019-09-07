eeff427d9c1f2a5410d1bc0f075bfe178d149e7b
andavid <jiankedai@gmail.com>
2019-09-07 20:06:02 +0800

    solve 226.invert-binary-tree

---
+++ vscode/226.invert-binary-tree.java
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=226 lang=java
+ *
+ * [226] Invert Binary Tree
+ *
+ * https://leetcode.com/problems/invert-binary-tree/description/
+ *
+ * algorithms
+ * Easy (59.32%)
+ * Likes:    1976
+ * Dislikes: 35
+ * Total Accepted:    358K
+ * Total Submissions: 601.9K
+ * Testcase Example:  '[4,2,7,1,3,6,9]'
+ *
+ * Invert a binary tree.
+ *
+ * Example:
+ *
+ * Input:
+ *
+ *
+ * ⁠    4
+ * ⁠  /   \
+ * ⁠ 2     7
+ * ⁠/ \   / \
+ * 1   3 6   9
+ *
+ * Output:
+ *
+ *
+ * ⁠    4
+ * ⁠  /   \
+ * ⁠ 7     2
+ * ⁠/ \   / \
+ * 9   6 3   1
+ *
+ * Trivia:
+ * This problem was inspired by this original tweet by Max Howell:
+ *
+ * Google: 90% of our engineers use the software you wrote (Homebrew), but you
+ * can’t invert a binary tree on a whiteboard so f*** off.
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public TreeNode invertTree(TreeNode root) {
+    if (root == null) {
+      return null;
+    }
+    TreeNode left = invertTree(root.right);
+    TreeNode right = invertTree(root.left);
+    root.left = left;
+    root.right = right;
+    return root;
+  }
+}
+
