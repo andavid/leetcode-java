a4b957702dc3abbca14172fe9ac4f98fe1cd42d0
andavid <jiankedai@gmail.com>
2019-09-08 17:12:35 +0800

    solve 543.diameter-of-binary-tree

---
+++ vscode/543.diameter-of-binary-tree.java
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=543 lang=java
+ *
+ * [543] Diameter of Binary Tree
+ *
+ * https://leetcode.com/problems/diameter-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (47.31%)
+ * Likes:    1647
+ * Dislikes: 102
+ * Total Accepted:    154.9K
+ * Total Submissions: 327K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ *
+ * Given a binary tree, you need to compute the length of the diameter of the
+ * tree. The diameter of a binary tree is the length of the longest path
+ * between any two nodes in a tree. This path may or may not pass through the
+ * root.
+ *
+ *
+ *
+ * Example:
+ * Given a binary tree
+ *
+ * ⁠         1
+ * ⁠        / \
+ * ⁠       2   3
+ * ⁠      / \
+ * ⁠     4   5
+ *
+ *
+ *
+ * Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+ *
+ *
+ * Note:
+ * The length of path between two nodes is represented by the number of edges
+ * between them.
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  int max = 0;
+
+  public int diameterOfBinaryTree(TreeNode root) {
+    maxDepth(root);
+    return max;
+  }
+
+  public int maxDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+
+    int left = maxDepth(root.left);
+    int right = maxDepth(root.right);
+
+    max = Math.max(max, left + right);
+
+    return Math.max(left, right) + 1;
+  }
+}
+
