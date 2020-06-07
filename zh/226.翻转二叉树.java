2cfc16f3c00de2742aed09856c82d337803be069
andavid <jiankedai@gmail.com>
2020-06-07 22:48:23 +0800

    solve 226.翻转二叉树

---
+++ zh/226.翻转二叉树.java
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=java
+ *
+ * [226] 翻转二叉树
+ *
+ * https://leetcode-cn.com/problems/invert-binary-tree/description/
+ *
+ * algorithms
+ * Easy (75.17%)
+ * Likes:    462
+ * Dislikes: 0
+ * Total Accepted:    83.9K
+ * Total Submissions: 111.4K
+ * Testcase Example:  '[4,2,7,1,3,6,9]'
+ *
+ * 翻转一棵二叉树。
+ *
+ * 示例：
+ *
+ * 输入：
+ *
+ * ⁠    4
+ * ⁠  /   \
+ * ⁠ 2     7
+ * ⁠/ \   / \
+ * 1   3 6   9
+ *
+ * 输出：
+ *
+ * ⁠    4
+ * ⁠  /   \
+ * ⁠ 7     2
+ * ⁠/ \   / \
+ * 9   6 3   1
+ *
+ * 备注:
+ * 这个问题是受到 Max Howell 的 原问题 启发的 ：
+ *
+ * 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public TreeNode invertTree(TreeNode root) {
+    if (root == null) {
+      return root;
+    }
+    TreeNode left = invertTree(root.right);
+    TreeNode right = invertTree(root.left);
+    root.left = left;
+    root.right = right;
+    return root;
+  }
+}
+// @lc code=end
+
