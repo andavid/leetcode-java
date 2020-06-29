d1ca3687c6749cf36ab1b9246d42da96ed554ad3
andavid <jiankedai@gmail.com>
2020-06-29 22:06:07 +0800

    solve 111.二叉树的最小深度

---
+++ zh/111.二叉树的最小深度.java
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode.cn id=111 lang=java
+ *
+ * [111] 二叉树的最小深度
+ *
+ * https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (42.41%)
+ * Likes:    277
+ * Dislikes: 0
+ * Total Accepted:    85.7K
+ * Total Submissions: 200.5K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，找出其最小深度。
+ *
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例:
+ *
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ *
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ *
+ * 返回它的最小深度  2.
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public int minDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    int depth = 1;
+
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      for (int i = 0; i < size; i++) {
+        TreeNode node = queue.poll();
+        if (node.left == null && node.right == null) {
+          return depth;
+        }
+        if (node.left != null) {
+          queue.offer(node.left);
+        }
+        if (node.right != null) {
+          queue.offer(node.right);
+        }
+      }
+      depth++;
+    }
+
+    return depth;
+  }
+}
+// @lc code=end
+
