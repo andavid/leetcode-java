d2c65be5062e84484b164504a87940b50bb4cbd6
andavid <jiankedai@gmail.com>
2020-07-08 10:44:43 +0800

    solve 35.搜索插入位置

---
+++ zh/35.搜索插入位置.java
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=java
+ *
+ * [35] 搜索插入位置
+ *
+ * https://leetcode-cn.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (45.58%)
+ * Likes:    550
+ * Dislikes: 0
+ * Total Accepted:    179.4K
+ * Total Submissions: 391.8K
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ *
+ * 你可以假设数组中无重复元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ *
+ *
+ * 示例 3:
+ *
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ *
+ *
+ * 示例 4:
+ *
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ *
+ *
+ */
+
+// @lc code=start
+class Solution {
+  public int searchInsert(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] < target) {
+        left = mid + 1;
+      } else if (nums[mid] > target) {
+        right = mid - 1;
+      } else if (nums[mid] == target) {
+        return mid;
+      }
+    }
+    return left;
+  }
+}
+// @lc code=end
+
